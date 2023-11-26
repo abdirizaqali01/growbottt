455006ab570a6a5edff5fbb1fd31459390d8898b
abdirizaqali01 <117480523+abdirizaqali01@users.noreply.github.com>
2023-11-26 16:35:40 +0200

    Update settings.py

--- growbotai2/settings.py
+++ growbotai2/settings.py
@@ -129,3 +129,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+
+django_heroku.settings(locals())
