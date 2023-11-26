a8517aa8359f3c6496a97d6cebebbde937fbcabe
abdirizaqali01 <117480523+abdirizaqali01@users.noreply.github.com>
2023-11-26 16:41:36 +0200

    Update wsgi.py

--- growbotai2/wsgi.py
+++ growbotai2/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'template.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'growbotai2.settings')
 
 application = get_wsgi_application()
 application = WhiteNoise(application)
